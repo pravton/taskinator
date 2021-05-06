67aabef516fc5733f1e369b04bff90f83831cad5
PravTon <clintonsf@outlook.com>
2021-05-06 10:32:12 -0400

    added form and event handler for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#task-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     taskToDoEl.appendChild(listItemEl);
+    console.log("what is happening");
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
